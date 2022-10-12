f5eb5732728bbad1732d432d33fa1cd20f7d5db8
Lukman Hakim <52951753+letman16@users.noreply.github.com>
2022-10-12 15:04:22 +0700

    Add files via upload

---
+++ P4/latihan/index.js
@@ -0,0 +1,9 @@
+import AuthorData from './author_data.js'
+let authorData = new AuthorData()
+let show = function() {
+    console.log(authorData);
+    authorData.getProfile().then(data => {
+        authorData.showProfile(data);
+    }).catch(e => console.log(e));
+}
+show()
